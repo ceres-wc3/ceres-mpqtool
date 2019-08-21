e0c05a0997972b0d3e5f048d2c478feb4965415c
mori <mori@reu.moe>
2019-08-21 16:07:08 +0400

    Change travis release options

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -18,7 +18,7 @@ main() {
     test -f Cargo.lock || cargo generate-lockfile
 
     # TODO Update this to build the artifacts that matter to you
-    cross rustc --bin mpqtool --target $TARGET --release -- -C lto
+    cross rustc --bin mpqtool --target $TARGET --release -- -C lto -C codegen-units=1
 
     # TODO Update this to package the right artifacts
     cp target/$TARGET/release/mpqtool $stage/
